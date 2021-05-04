a46a51435d82073f212777c6ce860de315fbaadf
Egor Lukin-Diachkov <egorlkn@gmail.com>
2021-05-04 22:55:30 +0200

    Add Twig and setup homepage

--- src/Context/Frontend/RequestHandler.php
+++ src/Context/Frontend/RequestHandler.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace App\Context\Frontend;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RequestHandler
+class RequestHandler extends AbstractController
 {
     /**
      * @Route(path="/", name="homepage")
      */
     public function homepage(Request $request): Response
     {
-        return new Response('Hello world!');
+        return $this->render('homepage.html.twig');
     }
 }
